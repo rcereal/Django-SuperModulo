e18e06fdc9dcc5b8d05f378f9610111bc923f3e3
Ryan Vitor <ryanvitor330@gmail.com>
2024-09-11 21:46:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django-SuperModulo
+ Aulas Super Modulo de Django Infinity
---
+++ Testes Super Modulo/COMO CRIAR O AMBIENTE E ATIVAR.py
@@ -0,0 +1,5 @@
+# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned  PRIMEIRO COLA ISSO NO TERMINAL
+
+# Criar ambiente: python -m venv nomeDoAmbienteVirtual
+
+# Ativar ambiente: nomeDoAbiente\Scripts\activate
